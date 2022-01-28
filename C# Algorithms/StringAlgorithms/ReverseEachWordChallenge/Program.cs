876ae5176886a695218cf3c63051b3b9fd4a2845
redTreeofPlains01 <heekim3199@gmail.com>
2022-01-29 10:42:44 +1100

    challenge completed

--- C# Algorithms/StringAlgorithms/ReverseEachWordChallenge/Program.cs
+++ C# Algorithms/StringAlgorithms/ReverseEachWordChallenge/Program.cs
@@ -4,18 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace ReverseEachWordChallenge
 {
     class Program
     {
+        //In this challenge, you'll create an algorithm that reverses each word in a sentence. The input to the function will be a string and the output will be a new string where each word is reversed. There will be no punctuation and each word will be separated by spaces. Casing should remain the same. If a given letter was uppercase, it should remain uppercase in the reversal.
 
-        static String ReverseEachWord(String input)
+        /*static String ReverseEachWord(String input)
         {
             String result = Regex.Replace(input, @"\p{P}", "").Trim(); 
             //input.Trim().Replace(",", "");
             string arr = Reverse(result);
             return arr;
+        }*/
+
+
+        static String ReverseEachWord(String input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+            StringBuilder result = new StringBuilder();
+            String[] arr = input.Split(" ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result.Append(Reverse(arr[i]));
+                if(i != arr.Length - 1)
+                {
+                    result.Append(" ");    
+                }
+            }
+            return result.ToString();
         }
 
         static String Reverse(String input)
