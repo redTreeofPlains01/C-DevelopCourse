8dd617c4bc496b08bd9beeaff109f6394bc45684
redTreeofPlains01 <heekim3199@gmail.com>
2022-02-23 16:24:39 +1100

    linkedlist

--- C# Algorithms/Ex_Files_C_Sharp_Algorithms/03/03_02/start/Algorithms/Program.cs
+++ C# Algorithms/Ex_Files_C_Sharp_Algorithms/03/03_02/start/Algorithms/Program.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Algorithms {
+
+    //Common Linked list operations in C#
     class Program {
 
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World");
+           LinkedList<string> linky = new LinkedList<string>();
+            //AddFirst
+            //AddLast
+            linky.AddFirst("Sarah");
+            linky.AddLast("Polly");
+            linky.AddLast("Rebceca");
+            linky.AddLast("Jess");
+            linky.AddLast("Gene");
+
+
+            Console.WriteLine(linky.Contains("Polly"));
+            Console.WriteLine(linky.Count);
+
+            linky.RemoveFirst();    
+
+            foreach (string link in linky) {
+                Console.Write(link + "->");
+            }
         }
     }
 }
