8f6b63c5b4e95e3b2d4781d8d7802375f0948bcd
redTreeofPlains01 <heekim3199@gmail.com>
2022-02-25 14:27:23 +1100

    na

--- C# Algorithms/Ex_Files_C_Sharp_Algorithms/03/03_03/start/Algorithms/Program.cs
+++ C# Algorithms/Ex_Files_C_Sharp_Algorithms/03/03_03/start/Algorithms/Program.cs
@@ -1,19 +1,66 @@
 ﻿using System;
 
 namespace Algorithms {
+
+
     class CustomLinkedList {
 
         Node head;
 
         public class Node {
             public int data;
-            public Node next;
+            public Node next;//?? declaration of a node class with the same class-almost seems recursive
 
             public Node(int d) { data = d; }
         }
+        public void deleteBackHalf()
+        {
+            //in case there is null data or 1 data
+            if (head == null || head.next == null)
+            {
+                head = null;
+            }
 
+            Node slow = head;
+            Node fast = head;
+            Node prev = null;
+
+            //reaching the end
+            while (fast != null && fast.next !=null )
+            {
+                prev = slow;
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            prev.next = null;
+        }
+        public void displayContents()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.data + "->");
+                current = current.next;
+            }
+        }
         static void Main(string[] args) {
+            CustomLinkedList linkedlist = new CustomLinkedList();
+            Node firstNode = new Node(3);
+            Node secondNode = new Node(4);
+            Node thirdNode = new Node(5);
+            Node fourthNode = new Node(6);
 
+            linkedlist.head = firstNode;
+            firstNode.next = secondNode;
+            secondNode.next = thirdNode;
+            thirdNode.next = fourthNode;
+            //fourthNode.next = null;
+
+            linkedlist.displayContents();
+            linkedlist.deleteBackHalf();
+            Console.WriteLine();
+            linkedlist.displayContents();
         }
     }
 }
