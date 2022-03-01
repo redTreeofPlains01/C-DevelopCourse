ea16f6c78207eb9656f58ceafb372e7d886f4b5d
redTreeofPlains01 <heekim3199@gmail.com>
2022-03-01 11:45:43 +1100

    hashset

--- C# Algorithms/Ex_Files_C_Sharp_Algorithms/05/05_02/start/Algorithms/Employee.cs
+++ C# Algorithms/Ex_Files_C_Sharp_Algorithms/05/05_02/start/Algorithms/Employee.cs
@@ -14,10 +14,33 @@ namespace Algorithms {
             this.department = department;
         }
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             Employee employee = new Employee("Robby", 3827, "Technology");
             Employee employee2 = new Employee("Bobby", 9415, "Marketing");
             Employee employee3 = new Employee("Sally", 2519, "Sales");
+
+            Dictionary<int, Employee> EmployeesById = new Dictionary<int, Employee>();
+            EmployeesById.Add(employee.id, employee);
+            EmployeesById.Add(employee2.id, employee2);
+            EmployeesById.Add(employee3.id, employee3);
+
+            //TryGetValue has a "sideeffect"of the fx- out value : out if true
+            Employee e;
+            if (EmployeesById.TryGetValue(9415, out e))
+            {
+                Console.WriteLine(e.name + e.department);
+            }
+            Console.WriteLine("Hello");
+
+            //Hashset is useful to see if something is contianed in a group 
+            HashSet<string> productNo = new HashSet<string>();
+            productNo.Add("ED879797");
+            productNo.Add("RT879TY7");
+            productNo.Add("ET876797");
+
+            productNo.Contains("RT879TY7");
+            productNo.Contains("hg");
         }
     }
 }
--- C# Algorithms/Ex_Files_C_Sharp_Algorithms/05/05_03/start/Algorithms/Program.cs
+++ C# Algorithms/Ex_Files_C_Sharp_Algorithms/05/05_03/start/Algorithms/Program.cs
@@ -4,8 +4,39 @@ using System.Collections.Generic;
 namespace Algorithms {
     class Program {
 
-        static void Main(string[] args) {
+        //make a empty missing list
+        //copy the second list to a hash set. 
+        //compare the fiorst list to the hash set
+        //if ! in hashset add to missinglist
+        private static List<int> FindMissingElements(List<int> firstList, List<int> secondList) {
+            List<int> missingList = new List<int>();
+            HashSet<int> second = new HashSet<int>();
+            foreach (int i in secondList) {
+                second.Add(i);
+            }
+            foreach (int i in firstList) {
+                if (!second.Contains(i)) {
+                    missingList.Add(i);
+                }
+                
+            }
+            return missingList;
+        }
 
+
+        static void Main(string[] args)
+        {
+
+            List<int> list1 = new List<int> { 1, 2, 3, 4 };
+            List<int> list2 = new List<int> { 3, 4 };
+
+            List<int> missingElements = FindMissingElements(list1, list2);
+
+            foreach (int num in missingElements)
+            {
+                Console.WriteLine(num);
+
+            }
         }
     }
 }
