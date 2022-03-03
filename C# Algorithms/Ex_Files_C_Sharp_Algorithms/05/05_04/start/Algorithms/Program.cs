53f3e67ecdc844f44ab384f072317b44af52ca62
redTreeofPlains01 <heekim3199@gmail.com>
2022-03-03 16:03:34 +1100

    kvp

--- C# Algorithms/Ex_Files_C_Sharp_Algorithms/05/05_04/start/Algorithms/Program.cs
+++ C# Algorithms/Ex_Files_C_Sharp_Algorithms/05/05_04/start/Algorithms/Program.cs
@@ -4,7 +4,69 @@ using System.Collections.Generic;
 namespace Algorithms {
     class Program {
 
+        static void displayFrequencyOfeachElement(int[] array)
+        {
+            //create a dictionary list
+            Dictionary<int, int> Freq = new Dictionary<int, int>();
+
+            //go thru the array list
+            for (int i = 0; i < array.Length; i++)
+            {//check if array value is a key of the Freq-dictionary
+
+                //if No make a new key pair in the Freq-dictionary, with value 1
+                //!!-Error: 
+                /*if (array[i] != Freq[array[i]])
+                {
+                    Freq[i] = 1;
+                }
+                //if yes increase the value
+                else if (array[i] == Freq[i])
+                {
+                    Freq[i]++;
+                }*/
+
+                //if yes increase the value
+                if (Freq.ContainsKey(array[i]))
+                {
+                    Freq[array[i]]++;
+                }
+                else
+                {
+                    Freq[array[i]] = 1;
+                }
+
+                //else add keyvalue pair
+            }
+            //return the freq-Dictionary
+            //print directly instread and make method void
+            //return Freq;
+            foreach (KeyValuePair<int, int> kvp in Freq)
+            {
+                Console.WriteLine(kvp.Key + "->" + kvp.Value);
+            }
+        }
+
         static void Main(string[] args) {
+
+            //make an array
+            //int[] elements = new int[] { 4, 7, 7, 5, 6, 8, 8, 8, 3, 2, 1 };
+            
+            //Dictionary<int,int> displayList = new Dictionary<int, int>();
+
+            //call the displayFrequencyOfeachElement method
+            //displayList= displayFrequencyOfeachElement(elements);
+
+
+            //Combnine above : as a challemnge call the method with an array
+            /*displayList= displayFrequencyOfeachElement(new int[] { 4, 7, 7, 5, 6, 8, 8, 8, 3, 2, 1 });*/
+            displayFrequencyOfeachElement(new int[] { 4, 7, 7, 5, 6, 8, 8, 8, 3, 2, 1 });
+
+            //Print the result to console using a foreach(kvp)loop
+            /*foreach (KeyValuePair<int, int> kvp in displayList)
+            {
+                Console.WriteLine(kvp.Key + "->" + kvp.Value);
+            }*/
+
         }
     }
 }
